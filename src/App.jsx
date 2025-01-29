f9f89ea21b5ec2bcd7494a1ca1a731383595e71c
Pedro Lavin <pacuevasl@gmail.com>
2025-01-29 19:11:05 -0300

    feat: service API with axios

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ListTasks from "./components/ListTasks"
+
+export default function App () {
+    return(
+        <>
+        <div>
+        <ListTasks />
+        </div>
+        </>
+    )
+}
